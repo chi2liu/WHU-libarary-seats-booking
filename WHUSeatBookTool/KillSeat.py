999fa8869b772dea684aeba4caee8eb9eb54b9a9
chi liu <cliu_whu@yeah.net>
2018-01-07 21:02:26 +0800

    prevent the exe exit quick

--- WHUSeatBookTool/KillSeat.py
+++ WHUSeatBookTool/KillSeat.py
@@ -2,7 +2,7 @@ from SeatRobber import Robber
 
 
 def get_username_password():
-    name = input('请输入您的用户名：')
+    name = input('请输入您的图书馆帐号：')
     pwd = input('请输入您的密码：')
     email_address = input('请输入您接受预约结果通知的邮箱地址（如：123456@qq.com）：')
     return name, pwd, email_address
@@ -13,3 +13,5 @@ if __name__ == '__main__':
     username, password, address = get_username_password()
     robber = Robber(username=username, password=password, address=address)
     robber.robber_seat()
+    # 避免打包exe之后，执行完cmd立即退出
+    input()
