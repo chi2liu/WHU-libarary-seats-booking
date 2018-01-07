5eb27939a6aca2f897ab94b9918482239d6f0844
chi liu <cliu_whu@yeah.net>
2018-01-07 15:26:51 +0800

    update

--- WHUSeatBookTool/main.py
+++ WHUSeatBookTool/main.py
@@ -1,7 +1,14 @@
 from SeatRobber import Robber
 
+
+def get_username_password():
+    name = input('请输入您的用户名：')
+    pwd = input('请输入您的密码：')
+    return name, pwd
+
+
 if __name__ == '__main__':
-	username = input('请输入用户名：')
-	password = input('请输入密码：')
+    print('>--------武汉大学抢座神器------by 633--<')
+    username, password = get_username_password()
     robber = Robber(username=username, password=password)
     robber.robber_seat()
